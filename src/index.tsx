ab2a9e681fd8869573816d75e4e9af63f51ef417
Joe Nudell <jnu@stanford.edu>
2022-12-09 13:31:20 -0800

    boilerplate

---
+++ src/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+import {App} from './App';
+
+// Create root if the template doesn't already contain it.
+let rootEl = document.getElementById('root');
+if (!rootEl) {
+  rootEl = document.createElement('div')
+  rootEl.id = 'root';
+  document.body.appendChild(rootEl);
+}
+
+// Render app
+const root = ReactDOM.createRoot(rootEl);
+root.render(<App />);
---
+++ webpack.config.js
@@ -0,0 +1,59 @@
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+
+const PROD = process.env.NODE_ENV === 'production';
+
+module.exports = {
+  entry: './src/index.tsx',
+  mode: PROD ? 'production' : 'development',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.scss$/i,
+        use: [
+          PROD ? MiniCssExtractPlugin.loader : 'style-loader',
+          'css-loader',
+          'sass-loader',
+        ],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  devtool: 'source-map',
+  devServer: {
+    port: 8181,
+    historyApiFallback: true,
+    hot: true,
+    allowedHosts: 'auto',
+    client: {
+      logging: 'info',
+      overlay: true,
+      progress: true,
+      reconnect: 20,
+    },
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'CatGPT3',
+    }),
+  ].concat(PROD ? [
+    new MiniCssExtractPlugin({filename: 'kindle.[hash].css'})
+  ] : []),
+  output: {
+    filename: PROD ? 'kindle.[chunkhash].js' : 'kindle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  optimization: {
+    minimize: PROD,
+    minimizer: [new CssMinimizerPlugin(), '...'],
+  },
+};
